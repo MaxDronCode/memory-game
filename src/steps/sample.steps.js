c6f275d1e4866e2ecccbd566a28c7c5b60eddfec
Max Dron <maxdron@icloud.com>
2024-07-22 12:56:23 +0200

    CONFIG: Config testing

---
+++ src/steps/sample.steps.js
@@ -0,0 +1,10 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Page from '../app/page'
+export function openThePage () {
+  render(<Page />)
+}
+export function getTitle () {
+  const title = screen.getByTestId('app-title')
+  return title.innerHTML
+}
