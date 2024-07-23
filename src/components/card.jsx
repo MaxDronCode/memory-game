86498d555c12fbeabb5925ce70dfbb742c18fa12
Max Dron <maxdron@icloud.com>
2024-07-23 10:26:31 +0200

    FEATURE: Starting the Game - All cards should be covered

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,7 @@
 import '@/styles/card.css'
 export default function Card ({ logo }) {
   return (
-    <div className='card'>
+    <div className='card covered'>
       <img src={logo} alt='Logo' />
     </div>
   )
