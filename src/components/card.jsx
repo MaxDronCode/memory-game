2caec3fbdd36b1a1aa34a922e72f0e18bbada983
Max Dron <maxdron@icloud.com>
2024-07-23 10:34:02 +0200

    PROJECT: FIX Card component, now it uses useState() and adding 'use client' to Grid Component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,14 @@
 import '@/styles/card.css'
+import { useState } from 'react'
+
 export default function Card ({ logo }) {
+  const [isCovered, setIsCovered] = useState(true)
+
   return (
-    <div className='card covered'>
+    <div className={
+      'card' + (isCovered ? ' covered' : '')
+    }
+    >
       <img src={logo} alt='Logo' />
     </div>
   )
