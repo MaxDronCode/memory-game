d152882f723f07ab1d8f635fe76c0515f77c8e48
Max Dron <maxdron@icloud.com>
2024-07-23 09:22:27 +0200

    PROJECT: Create Card Component and Download / Import Logos

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import Card from '@/components/card'
+
 export default function Home () {
   return (
-    <h1 data-testid='app-title'>Hello React!</h1>
+    <>
+      <h1 data-testid='app-title'>Hello React!</h1>
+      <Card />
+    </>
   )
 }
---
+++ src/components/card.jsx
@@ -0,0 +1,7 @@
+export default function Card ({ logo }) {
+  return (
+    <div className='card'>
+      <img src={logo} alt='Logo' />
+    </div>
+  )
+}
