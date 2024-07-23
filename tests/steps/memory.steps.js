f160f445d4931431b6db2b04ac7f72cc0ab2de12
Max Dron <maxdron@icloud.com>
2024-07-23 10:25:25 +0200

    STEPS: Starting the Game - All cards should be covered

--- tests/steps/memory.steps.js
+++ tests/steps/memory.steps.js
@@ -14,3 +14,13 @@ export function checkGridSize (numRows, numColumns) {
   const cards3row = rows[2].children
   return rows.length === Number(numRows) && cards1row.length === Number(numColumns) && cards2row.length === Number(numColumns) && cards3row.length === Number(numColumns)
 }
+
+export function allCardsCovered () {
+  const grid = screen.getByTestId('grid')
+  const rows = grid.children
+  const cards1row = rows[0].children
+  const cards2row = rows[1].children
+  const cards3row = rows[2].children
+  const allCards = [...cards1row, ...cards2row, ...cards3row]
+  return allCards.every(card => card.classList.contains('covered'))
+}
