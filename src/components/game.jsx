24dee7c98f0f1c397acad5a365affea1e6ce8a40
Max Dron <maxdron@icloud.com>
2024-07-24 09:05:37 +0200

    PROJECT: Implement Game and MockDataForm Components

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,11 +1,37 @@
+'use client'
 import Grid from './grid'
 import '@/styles/game.css'
+import MockDataForm from './mockDataForm'
+import { useState, useEffect } from 'react'
 
 export default function Game () {
+  const [mockDataFormVisible, setMockDataFormVisible] = useState(false)
+  const [mockData, setMockData] = useState('')
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress)
+    }
+  }, [handleKeyPress])
+
+  function handleKeyPress (e) {
+    if (e.ctrlKey && e.key.toUpperCase() === 'M') {
+      setMockDataFormVisible(!mockDataFormVisible)
+    }
+  }
+
+  function setMockDataForm (data) {
+    setMockData(data)
+    setMockDataFormVisible(false)
+  }
+
   return (
     <div className='game'>
       <h1>Memory Game</h1>
-      <Grid />
+      {mockDataFormVisible && <MockDataForm setData={setMockDataForm} />}
+      <Grid mockData={mockData} />
     </div>
   )
 }
