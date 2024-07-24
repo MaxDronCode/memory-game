e0d2aef27eb979433d4caaf63e689c2b5b435e9c
Max Dron <maxdron@icloud.com>
2024-07-24 11:29:01 +0200

    STEPS: Uncovering a card - Disabling the card

--- tests/steps/memory.steps.js
+++ tests/steps/memory.steps.js
@@ -34,3 +34,39 @@ export function allCardsEnabled () {
   const allCards = [...cards1row, ...cards2row, ...cards3row]
   return allCards.every(card => !card.classList.contains('disabled'))
 }
+
+export function setMockData (data) {
+  data = data.trim()
+
+  fireEvent.keyDown(screen.getByTestId('grid'), {
+    key: 'm',
+    keyCode: 77,
+    which: 77,
+    code: 'KeyM',
+    location: 0,
+    altKey: false,
+    ctrlKey: true,
+    metaKey: false,
+    shiftKey: false,
+    repeat: false
+  })
+
+  const textInput = screen.getByTestId('mock-data-input')
+  const submitButton = screen.getByTestId('mock-data-submit')
+  fireEvent.change(textInput, { target: { value: data } })
+  fireEvent.click(submitButton)
+}
+
+export function uncoverCard (rowPosition, colPosition) {
+  const grid = screen.getByTestId('grid')
+  const rows = grid.children
+  const card = rows[rowPosition].children[colPosition]
+  fireEvent.click(card)
+}
+
+export function isCardUncovered (rowPosition, colPosition) {
+  const grid = screen.getByTestId('grid')
+  const rows = grid.children
+  const card = rows[rowPosition].children[colPosition]
+  return !card.classList.contains('covered')
+}
