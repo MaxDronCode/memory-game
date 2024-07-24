3719dc64750e23a00c80b80fc1cbb82e7689fa95
Max Dron <maxdron@icloud.com>
2024-07-24 11:55:50 +0200

    FEATURE: Uncovering a card - Disabling the card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,9 +3,27 @@ import { useState } from 'react'
 
 export default function Card ({ value }) {
   const [isCovered, setIsCovered] = useState(true)
+  const [isDisabled, setIsDisabled] = useState(false)
+
+  function changeStatus () {
+    if (!isDisabled) {
+      setIsCovered(!isCovered)
+      setIsDisabled(true)
+    }
+  }
+
+  function setClassName () {
+    let className = 'card'
+    if (isCovered) {
+      className += ' covered'
+    } else if (!isCovered && isDisabled) {
+      className += ' uncovered disabled'
+    }
+    return className
+  }
 
   return (
-    <div className={'card' + (isCovered ? ' covered' : '')} onClick={() => setIsCovered(!isCovered)}>
+    <div className={setClassName()} onClick={changeStatus}>
       <img src={value + '.svg'} alt='Logo' />
     </div>
   )
